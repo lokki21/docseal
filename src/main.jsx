c7118f9e43245398e182ac7ca3477b4e7b74ca6e
lokki21 <86375720+lokki21@users.noreply.github.com>
2026-02-13 16:43:43 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DocumentAuthenticator from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DocumentAuthenticator />
+  </React.StrictMode>,
+)
